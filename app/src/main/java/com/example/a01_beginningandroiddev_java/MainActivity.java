9625a8bc01e6c5d5e703fb0cf9a75e7b79766f93
will7hughes <will7hughes@gmail.com>
2019-10-04 13:26:55 -0500

    Initial

---
+++ app/src/main/java/com/example/a01_beginningandroiddev_java/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a01_beginningandroiddev_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
